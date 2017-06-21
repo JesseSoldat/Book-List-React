d737466e6ab5eecbd928426eebf557b9005e3277
jesse <jsoldat@hotmail.com>
2017-06-21 08:04:21 -0400

    can render a list of the books

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
+import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={createStore(reducers)}>
+	<App />
+</Provider>
+, document.getElementById('root'));
 registerServiceWorker();
